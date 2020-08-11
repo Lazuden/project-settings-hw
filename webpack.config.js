5b4a5a61479ed6a061ef244413ac6b803c533f8b
Lazuden <58232561+Lazuden@users.noreply.github.com>
2020-08-11 16:30:45 +0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => ({
+    entry: './src/index.jsx',
+    output: {
+        filename: 'main.js',
+        path: path.join(__dirname, 'dist')
+    },
+    devtool: argv.mode === "production" ? 'hidden-source-map' : 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new MiniCssPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+      })
+    ],
+    module: { 
+        rules: [
+            {
+                test: /\.jsx?/i,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpe?g)/i,
+                use: 'file-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        } 
+                    },
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        port: 9000,
+        contentBase: path.join(__dirname, 'dist'),
+        hot: true
+    }
+});
